e5ed921f2a3e824d7ef2aaa51da603f3ee4b6bcb
GabasDev <bieltoddy065@gmail.com>
2023-09-27 21:12:35 -0300

    implementacao do menu

--- main.c
+++ main.c
@@ -1,24 +1,59 @@
 #include "./src/cliente/cliente.c"
 #include "./src/veiculo/veiculo.c"
 
-int main()
-{
 
+int main() {
     Cliente *clientes = NULL;
     int id = 0;
-    printf("Cadastrando Felipe\n");
-    clientes = add_cliente(clientes, "Felipe", "84996579714", id);
-    printf("Cadastrando Abner\n");
-    clientes = add_cliente(clientes, "Abner", "84996579714", ++id);
-    printf("Cadastrando Whatsapp\n");
-    clientes = add_cliente(clientes, "Whatsapp", "84996579714", ++id);
-    printf("imprimindo nomes...\n\n");
-    imprime_cliente(clientes);
+    int escolha;
+    char nome[100];
+    char tel[15];
+    do {
+        printf("\n=== Menu Lava-Jato ===\n");
+        printf("1. Adicionar Cliente\n");
+        printf("2. Excluir Cliente\n");
+        printf("3. Listar Clientes\n");
+        printf("4. Buscar Cliente\n");
+        printf("5. Editar Informacoes do Cliente\n");
+        printf("6. Consultar Veiculos em Servico\n");
+        printf("7. Sair\n");
+        printf("Escolha uma opcao: \n");
+        scanf("%d", &escolha);
 
-    Cliente* exclui = excluir_cliente(clientes, 1);
-    printf("imprimindo nomes...\n\n");
-    imprime_cliente(exclui);
-    clientes_libera(clientes);
+        switch (escolha) {
+            case 1:
+                printf ("digite o nome do cliente \n");
+                scanf ("%s", &nome);
+                printf ("cadastrando %s\n", nome);
+                printf ("Digite o numero de telefone\n");
+                scanf ("%s", &tel);
+                printf ("Digite o id\n");
+                scanf ("%d", &id);
+                clientes = add_cliente(clientes, nome, &tel, id);
+                break;
+            case 2:
+                 clientes = excluir_cliente(clientes, 1);
+                break;
+            case 3:
+                imprime_cliente(clientes);
+                break;
+            case 4:
+               
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                
+                break;
+            case 7:
+                printf("Saindo do programa. Até logo!\n");
+                break;
+            default:
+                printf("Opção invalida. Tente novamente.\n");
+                break;
+        }
+    } while (escolha != 7);
 
     return 0;
 }
